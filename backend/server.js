5cd916ef62ee904caa7549957218bb9fe6b00dcd
Aditya Chatterjee <adityachatterjee1998ac@gmail.com>
2026-09-04 01:19:32 +0530

    backend port fix

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const Task = require("./models/Task");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
@@ -46,8 +47,6 @@ app.post("/api/tasks", async (req, res) => {
     });
   }
 });
-
-
 
 app.delete("/api/tasks/:id", async (req, res) => {
   try {
@@ -94,6 +93,6 @@ app.patch("/api/tasks/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
